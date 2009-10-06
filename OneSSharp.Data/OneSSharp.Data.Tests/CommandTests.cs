f25e121a21d328e1c4888d49fcf75495ee4445c3
Vsevolod Konkov <vsevolod.konkov@gmail.com>
2009-10-06 21:30:03 +0000

    Продолжение unit-тестов на OneSCommand забытые файлы.
    git-svn-id: http://localhost:8080/svn/one-s/trunk@10 807a2f40-92e0-6242-87b2-0573f1835f64

---
+++ OneSSharp.Data/OneSSharp.Data.Tests/CommandTests.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Data;
+using System.Data.Common;
+using System.Data.OleDb;
+
+namespace VsevolodKonkov.OneSSharp.Data.Tests
+{
+    /// <summary>Тестирование создания экземпляра класса <see cref="OneSCommand"/>.</summary>
+    [TestFixture(false, Description = "Тестирование команды запроса без открытия соединения")]
+    [TestFixture(true, Description = "Тестирование команды запроса с открытием соединения")]
+    public sealed class CommandTests : ConnectedTestsBase
+    {
+        /// <summary>Конструктор.</summary>
+        /// <param name="shouldBeOpen">Признак необходимости открытия соединения.</param>
+        public CommandTests(bool shouldBeOpen) : base(shouldBeOpen)
+        {}
+
+        /// <summary>Тестовый экземпляр команды.</summary>
+        private OneSCommand TestCommand
+        {
+            get { return _testCommand; }
+        }
+        private OneSCommand _testCommand;
+
+        /// <summary>Установка тестового окружения.</summary>
+        /// <remarks>Создание тестовой команды <see cref="TestCommand"/>.</remarks>
+        protected override void InternalSetUp()
+        {
+            base.InternalSetUp();
+
+            _testCommand = new OneSCommand(Connection);
+        }
+
+        /// <summary>Очистка окружения тестов.</summary>
+        /// <remarks>Очистка тестовой команды <see cref="TestCommand"/>.</remarks>
+        protected override void InternalTearDown()
+        {
+            if (_testCommand != null)
+                _testCommand.Dispose();
+
+            base.InternalTearDown();
+        }
+
+        /// <summary>Тестирование свойства <see cref="OneSCommand.CommandText"/>.</summary>
+        [Test(Description = "Тестирование свойства CommandText")]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("SELECT Справочник.Валюты")]
+        public void TestCommandTextProperty(string commandText)
+        {
+            TestCommand.CommandText = commandText;
+            Assert.AreEqual(commandText, TestCommand.CommandText);
+        }
+
+        /// <summary>Тестирование свойства <see cref="OneSCommand.CommandType"/>.</summary>
+        [Test(Description = "Тестирование значений свойства CommandType")]
+        [TestCase(CommandType.Text, Result = CommandType.Text)]
+        [TestCase(CommandType.TableDirect, ExpectedException = typeof(NotSupportedException))]
+        [TestCase(CommandType.StoredProcedure, ExpectedException = typeof(NotSupportedException))]
+        public CommandType TestCommandType(CommandType commandType)
+        {
+            TestCommand.CommandType = commandType;
+            return TestCommand.CommandType;
+        }
+
+        /// <summary>Тестирование свойства <see cref="OneSCommand.CommandTimeout"/>.</summary>
+        [Test(Description = "Тестирование свойства CommandTimeout")]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void TestCommandTimeout([Random(0, 10000, 5)] int value)
+        {
+            TestCommand.CommandTimeout = value;
+        }
+
+        /// <summary>Тестирование метода <see cref="OneSCommand.CreateParameter"/>.</summary>
+        [Test(Description = "Тестирование метода CreateParameter")]
+        public void TestCreateParameter()
+        {
+            Assert.IsNotNull(TestCommand.CreateParameter());
+        }
+
+        /// <summary>Тестирование метода <see cref="DbCommand.CreateParameter"/>.</summary>
+        [Test(Description = "Тестирование метода DbCommand.CreateParameter")]
+        public void TestDbCommandCreateParameter()
+        {
+            DbCommand dbCommand = TestCommand;
+            var result = dbCommand.CreateParameter();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<OneSParameter>(result);
+        }
+
+        /// <summary>Тестирование свойства <see cref="OneSCommand.Connection"/>.</summary>
+        [Test(Description = "Тестирование свойства Connection")]
+        public void TestConnection()
+        {
+            Assert.AreSame(Connection, TestCommand.Connection);
+
+            using (var testConnection = new OneSConnection())
+            {
+                TestCommand.Connection = testConnection;
+                Assert.AreSame(testConnection, TestCommand.Connection);
+            }
+
+            TestCommand.Connection = null;
+            Assert.IsNull(TestCommand.Connection);
+        }
+
+        /// <summary>Тестирование свойства <see cref="DbCommand.Connection"/>.</summary>
+        [Test(Description = "Тестирование свойства DbCommand.Connection")]
+        public void TestDbConnection()
+        {
+            DbCommand dbCommand = TestCommand;
+
+            Assert.AreSame(Connection, dbCommand.Connection);
+
+            using (var testConnection = new OneSConnection())
+            {
+                dbCommand.Connection = testConnection;
+                Assert.AreSame(testConnection, dbCommand.Connection);
+            }
+
+            dbCommand.Connection = null;
+            Assert.IsNull(TestCommand.Connection);
+
+            using (var oleDbConnection = new OleDbConnection())
+                Assert.Throws<ArgumentException>(() => dbCommand.Connection = oleDbConnection);
+        }
+
+        /// <summary>Тестирование свойства <see cref="DbCommand.Parameters"/>.</summary>
+        [Test(Description = "Тестирование свойства DbCommand.Parameters")]
+        public void TestDbParameters()
+        {
+            DbCommand dbCommand = TestCommand;
+
+            Assert.IsNotNull(dbCommand.Parameters);
+            Assert.IsInstanceOf<OneSParameterCollection>(dbCommand.Parameters);
+        }
+
+        /// <summary>Тестирование свойства <see cref="OneSCommand.DesignTimeVisible"/>.</summary>
+        [Test(Description = "Тестирование свойства DesignTimeVisible")]
+        public void TestDesignTimeVisible([Values(false, true)] bool value)
+        {
+            TestCommand.DesignTimeVisible = value;
+            Assert.AreEqual(value, TestCommand.DesignTimeVisible);
+        }
+
+        /// <summary>Тестирование свойства <see cref="OneSCommand.UpdatedRowSource"/>.</summary>
+        [Test(Description = "Тестирование свойства UpdatedRowSource")]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void TestUpdatedRowSource(
+            [Values(UpdateRowSource.None, UpdateRowSource.Both, UpdateRowSource.FirstReturnedRecord, UpdateRowSource.OutputParameters)] UpdateRowSource value)
+        {
+            TestCommand.UpdatedRowSource = value;
+        }
+    }
+}
---
+++ OneSSharp.Data/OneSSharp.Data.Tests/ConnectedTestsBase.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace VsevolodKonkov.OneSSharp.Data.Tests
+{
+    /// <summary>Базовый класс тестов требующие подключения.</summary>
+    public abstract class ConnectedTestsBase : TestsBase
+    {
+        /// <summary>Конструктор.</summary>
+        /// <param name="shouldBeOpen">Признак необходимости открытия соединения.</param>
+        protected ConnectedTestsBase(bool shouldBeOpen)
+        {
+            _shouldBeOpen = shouldBeOpen;
+        }
+
+        /// <summary>Конструктор.</summary>
+        protected ConnectedTestsBase() : this(true)
+        { }
+
+        /// <summary>Следует ли открывать соединение.</summary>
+        private readonly bool _shouldBeOpen;
+        
+        /// <summary>Тестовое соединение.</summary>
+        protected OneSConnection Connection
+        {
+            get { return _connection; }
+        }
+        private OneSConnection _connection;
+
+        /// <summary>Установка тестового окружения.</summary>
+        [SetUp]
+        public void SetUp()
+        {
+            _connection = new OneSConnection(TestConnectionString);
+            if (_shouldBeOpen)
+                _connection.Open();
+
+            InternalSetUp();
+        }
+
+        /// <summary>Очистка тестового окружения.</summary>
+        [TearDown]
+        public void TearDown()
+        {
+            InternalTearDown();
+
+            if (_connection != null)
+            {
+                _connection.Dispose();
+                _connection = null;
+            }
+        }
+
+        /// <summary>Установка окружения тестов.</summary>
+        /// <remarks>Точка расширения для наследных классов.</remarks>
+        protected virtual void InternalSetUp()
+        {}
+
+        /// <summary>Очистка окружения тестов.</summary>
+        /// <remarks>Точка расширения для наследных классов.</remarks>
+        protected virtual void InternalTearDown()
+        {}
+    }
+}
