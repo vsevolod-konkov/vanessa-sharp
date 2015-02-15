df3aad8db64906b7a49b2fcd1abcb2b652124bd2
Vsevolod Konkov <vsevolod.konkov@gmail.com>
2015-02-15 18:07:51 +0000

    Выпуск версии 0.2.0
    git-svn-id: http://localhost:8080/svn/one-s/trunk@134 807a2f40-92e0-6242-87b2-0573f1835f64

--- ProductInfo.cs
+++ ProductInfo.cs
@@ -1,9 +1,9 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
 [assembly: AssemblyProduct("VanessaSharp")]
-[assembly: AssemblyCopyright("Copyright Vsevolod Konkov©  2009-2014")]
+[assembly: AssemblyCopyright("Copyright Vsevolod Konkov©  2009-2015")]
 [assembly: AssemblyCompany("Vsevolod Konkov")]
 [assembly: ComVisible(false)]
