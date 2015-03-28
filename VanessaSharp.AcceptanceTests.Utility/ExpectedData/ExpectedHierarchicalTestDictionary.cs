5d485138ea8a0bca6685c68546ee6ed85bfd6b90
Vsevolod Konkov <vsevolod.konkov@gmail.com>
2015-03-28 23:40:32 +0000

    VNS_SHRP-33. Предварительный коммит разработки приемочных тестов.
    git-svn-id: http://localhost:8080/svn/one-s/trunk@136 807a2f40-92e0-6242-87b2-0573f1835f64

--- VanessaSharp.AcceptanceTests.Utility/ExpectedData/ExpectedHierarchicalTestDictionary.cs
+++ VanessaSharp.AcceptanceTests.Utility/ExpectedData/ExpectedHierarchicalTestDictionary.cs
@@ -17,10 +17,152 @@ namespace VanessaSharp.AcceptanceTests.Utility.ExpectedData
 
         private static ExpectedHierarchicalTestDictionary[] GetExpectedDataArray()
         {
-            return new ExpectedHierarchicalTestDictionary[]
+            return new []
                 {
-                    
+                    new ExpectedHierarchicalTestDictionary
+                        {
+                            Name = "Одежда",
+                            Children = new []
+                                {
+                                    new ExpectedHierarchicalTestDictionary
+                                        {
+                                            Name = "Женская одежда и белье",
+                                            Children = new []
+                                                {
+                                                    new ExpectedHierarchicalTestDictionary
+                                                        {
+                                                            Name = "Гольфы Грация 2+2",
+                                                            Quantity = 150,
+                                                            Price = 65m
+                                                        },
+
+                                                    new ExpectedHierarchicalTestDictionary
+                                                        {
+                                                            Name = "Колготки Золотая Грация",
+                                                            Quantity = 300,
+                                                            Price = 152m
+                                                        },
+
+                                                    new ExpectedHierarchicalTestDictionary
+                                                        {
+                                                            Name = "Комплект Charmante",
+                                                            Quantity = 720,
+                                                            Price = 791m
+                                                        }
+                                                }
+                                        },
+
+                                    new ExpectedHierarchicalTestDictionary
+                                        {
+                                            Name = "Мужская одежда",
+                                            Children = new []
+                                                {
+                                                    new ExpectedHierarchicalTestDictionary
+                                                        {
+                                                            Name = "Носки Пингонс",
+                                                            Quantity = 1400,
+                                                            Price = 80m
+                                                        },
+
+                                                    new ExpectedHierarchicalTestDictionary
+                                                        {
+                                                            Name = "Пижама Peche Monnaie",
+                                                            Quantity = 380,
+                                                            Price = 3145m
+                                                        },
+
+                                                    new ExpectedHierarchicalTestDictionary
+                                                        {
+                                                            Name = "Халат Peche Monnaie",
+                                                            Quantity = 25,
+                                                            Price = 4120
+                                                        }
+                                                }
+                                        },
+
+                                    new ExpectedHierarchicalTestDictionary
+                                        {
+                                            Name = "Обувь",
+                                            Children = new[]
+                                                {
+                                                    new ExpectedHierarchicalTestDictionary
+                                                        {
+                                                            Name = "Балеринки ISOTONER",
+                                                            Quantity = 650,
+                                                            Price = 1020m
+                                                        },
+
+                                                    new ExpectedHierarchicalTestDictionary
+                                                        {
+                                                            Name = "Мокасины ISOTONER",
+                                                            Quantity = 450,
+                                                            Price = 1020m
+                                                        }
+                                                }
+                                        }
+                                }
+                        },
+
+                    new ExpectedHierarchicalTestDictionary
+                        {
+                            Name = "Продукты",
+                            Children = new[]
+                                {
+                                    new ExpectedHierarchicalTestDictionary
+                                        {
+                                            Name = "Бакалея",
+                                            Children = new[]
+                                                {
+                                                    new ExpectedHierarchicalTestDictionary
+                                                        {
+                                                            Name = "Гречка Мистраль",
+                                                            Quantity = 200,
+                                                            Price = 83m
+                                                        },
+
+                                                    new ExpectedHierarchicalTestDictionary
+                                                        {
+                                                            Name = "Фасоль Шебекинская Южная",
+                                                            Quantity = 1000,
+                                                            Price = 112m
+                                                        }
+                                                }
+                                        },
+
+                                    new ExpectedHierarchicalTestDictionary
+                                        {
+                                            Name = "Молочные продукты",
+                                            Children = new[]
+                                                {
+                                                    new ExpectedHierarchicalTestDictionary
+                                                        {
+                                                            Name = "Коктейль Новая деревня",
+                                                            Quantity = 500,
+                                                            Price = 90.20m
+                                                        },
+
+                                                    new ExpectedHierarchicalTestDictionary
+                                                        {
+                                                            Name = "Сливки Чистый Край",
+                                                            Quantity = 200,
+                                                            Price = 174m
+                                                        }
+                                                }
+                                        }
+                                }
+                        }
                 };
         }
+
+        [Field("Наименование")]
+        public string Name;
+
+        [Field("Количество", FieldType = typeof(double))] 
+        public int? Quantity;
+
+        [Field("Цена", FieldType = typeof(double))]
+        public decimal? Price;
+
+        public ExpectedHierarchicalTestDictionary[] Children;
     }
 }
--- VanessaSharp.Data.AcceptanceTests/OneSDataReaderTests/HierarchyDataReaderTests.cs
+++ VanessaSharp.Data.AcceptanceTests/OneSDataReaderTests/HierarchyDataReaderTests.cs
@@ -19,7 +19,14 @@ namespace VanessaSharp.Data.AcceptanceTests.OneSDataReaderTests
 
         /// <summary>Тестирование поведения по умолчанию.</summary>
         [Test]
-        public void TestDefault()
+        public void TestDefault(
+            [Values(
+                QueryResultIteration.Default,
+                QueryResultIteration.Linear,
+                QueryResultIteration.ByGroups,
+                QueryResultIteration.ByGroupsWithHierarchy
+            )]
+            QueryResultIteration queryResultIteration)
         {
             Test
                 .Sql(
@@ -32,16 +39,25 @@ namespace VanessaSharp.Data.AcceptanceTests.OneSDataReaderTests
                       ПО ОБЩИЕ, Ссылка ТОЛЬКО ИЕРАРХИЯ
                     "
                 )
-                .Execute(queryResultIteration: QueryResultIteration.ByGroups)
-                .Action(ctx => PrintRecords(ctx.TestedReader))
+                .Execute(queryResultIteration: queryResultIteration)
+                .Action(ctx => PrintRecords(queryResultIteration, ctx.TestedReader))
                 .BeginDefineExpectedDataFor<ExpectedHierarchicalTestDictionary>()
                 .AllRows
                 .EndDefineExpectedData
            .Run();
         }
 
-        private static void PrintRecords(OneSDataReader testedReader)
+        private static void PrintRecords(QueryResultIteration queryResultIteration, OneSDataReader testedReader)
         {
+            Console.WriteLine("QueryResultIteration = {0}", queryResultIteration);
+
+            PrintRecords(queryResultIteration, testedReader, 1);
+        }
+
+        private static void PrintRecords(QueryResultIteration queryResultIteration, OneSDataReader testedReader, int recursion)
+        {
+            Console.WriteLine("Recursion = {0}", recursion);
+
             while (testedReader.Read())
             {
                 Console.WriteLine("Наименование = {0}, Количество = {1}, Цена = {2}, Уровень = {3}, Группировка = {4}, Тип записи = {5}",
@@ -52,9 +68,124 @@ namespace VanessaSharp.Data.AcceptanceTests.OneSDataReaderTests
                     testedReader.GroupName,
                     testedReader.RecordType);
 
-                using (var descendantsReader = testedReader.GetDescendantsReader())
-                    PrintRecords(descendantsReader);
+                //using (var descendantsReader = testedReader.GetDescendantsReader(queryResultIteration))
+                //    PrintRecords(queryResultIteration, descendantsReader, recursion + 1);
             }
+        }
+
+        private static void PrintRecord(OneSDataReader testedReader)
+        {
+            Console.WriteLine("Наименование = {0}, Количество = {1}, Цена = {2}, Уровень = {3}, Группировка = {4}, Тип записи = {5}",
+                                testedReader.GetString(0),
+                                testedReader.IsDBNull(1) ? "" : (object)testedReader.GetInt32(1),
+                                testedReader.IsDBNull(2) ? "" : (object)testedReader.GetDecimal(2),
+                                testedReader.Level,
+                                testedReader.GroupName,
+                                testedReader.RecordType);
+        }
+
+        [Test]
+        public void TestLinear()
+        {
+            Test
+                .Sql(
+                    @"
+                      ВЫБРАТЬ Наименование, Количество, Цена
+                      ИЗ Справочник.ИерархическийСправочник
+                      ИТОГИ
+                      СУММА(Количество),
+                      СРЕДНЕЕ(Цена)
+                      ПО ОБЩИЕ, Ссылка ТОЛЬКО ИЕРАРХИЯ Как ТоварИерархия
+                    "
+                )
+                .Execute(queryResultIteration: QueryResultIteration.Linear)
+                .Action(ctx =>
+                {
+                    
+                    
+                    while (ctx.TestedReader.Read())
+                    {
+                        PrintRecord(ctx.TestedReader);
+
+                        
+                    }
+                })
+                .BeginDefineExpectedDataFor<ExpectedHierarchicalTestDictionary>()
+
+                .Field(d => d.Name)
+                .Field(d => d.Price)
+                .Field(d => d.Quantity)
+
+                .AllRows
+                .EndDefineExpectedData
+           .Run();
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Test
+                .Sql(
+                    @"
+                      ВЫБРАТЬ Наименование, Количество, Цена
+                      ИЗ Справочник.ИерархическийСправочник
+                      ИТОГИ
+                      СУММА(Количество),
+                      СРЕДНЕЕ(Цена)
+                      ПО Ссылка ТОЛЬКО ИЕРАРХИЯ Как ТоварИерархия
+                    "
+                )
+                .Execute(queryResultIteration: QueryResultIteration.ByGroups)
+                .Action(ctx =>
+                {
+                    while (ctx.TestedReader.Read())
+                        PrintRecord(ctx.TestedReader);
+                })
+                .BeginDefineExpectedDataFor<ExpectedHierarchicalTestDictionary>()
+                .AllRows
+                .EndDefineExpectedData
+           .Run();
+        }
+
+        [Test]
+        public void Test4()
+        {
+            Test
+                .Sql(
+                    @"
+                      ВЫБРАТЬ Наименование, Количество, Цена
+                      ИЗ Справочник.ИерархическийСправочник
+                      УПОРЯДОЧИТЬ ПО Ссылка ИЕРАРХИЯ
+                    "
+                )
+                .Execute(queryResultIteration: QueryResultIteration.ByGroupsWithHierarchy)
+                .Action(ctx =>
+                {
+                    while (ctx.TestedReader.Read())
+                    {
+                        PrintRecord(ctx.TestedReader);
+
+                        using (var descendantsReader = ctx.TestedReader.GetDescendantsReader(QueryResultIteration.ByGroupsWithHierarchy))
+                        {
+                            while (descendantsReader.Read())
+                            {
+                                PrintRecord(descendantsReader);
+
+                                using (var descendantsReader2 = descendantsReader.GetDescendantsReader(QueryResultIteration.ByGroupsWithHierarchy))
+                                {
+                                    while (descendantsReader2.Read())
+                                    {
+                                        PrintRecord(descendantsReader2);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                })
+                .BeginDefineExpectedDataFor<ExpectedHierarchicalTestDictionary>()
+                .AllRows
+                .EndDefineExpectedData
+           .Run();
         }
     }
 }
