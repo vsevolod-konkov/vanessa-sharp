34d538fe9727725dedf531ab451593a080c54689
Vsevolod Konkov <vsevolod.konkov@gmail.com>
2017-05-17 01:32:25 +0300

    Выпуск версии 0.4.0.

--- ProductInfo.cs
+++ ProductInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.3.2.0")]
-[assembly: AssemblyFileVersion("0.3.2.0")]
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
 [assembly: AssemblyProduct("VanessaSharp")]
 [assembly: AssemblyCopyright("Copyright Vsevolod Konkov©  2009-2017")]
 [assembly: AssemblyCompany("Vsevolod Konkov")]
